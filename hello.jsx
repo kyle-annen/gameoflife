cf744ce0a20bf02c9953c8ad84a20db95a9a9989
Kyle Annen <kyleannen@gmail.com>
2017-01-30 22:02:15 -0600

    finish all components views

--- hello.jsx
+++ hello.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Collapse, Modal, ModalHeader, ModalBody, ModalFooter, ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, 
+					Form,
+					FormGroup, 
+					Label, 
+					Input, 
+					FormText, 
+					Collapse, 
+					Modal, 
+					ModalHeader, 
+					ModalBody, 
+					ModalFooter, 
+					ListGroup, 
+					ListGroupItem 
+				} from 'reactstrap';
 
 class RecipeBox extends React.Component {
 	constructor(props) {
@@ -54,9 +67,10 @@ class RecipeBox extends React.Component {
 				<Modal isOpen={this.state.modal} toggle={this.close} >
           <ModalHeader toggle={this.close}>Add Recipe</ModalHeader>
           <ModalBody>
+          	<RecipeForm />
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.close}>Do Something</Button>{' '}
+            <Button color="primary" onClick={this.close}>Save Recipe</Button>
             <Button color="secondary" onClick={this.close}>Cancel</Button>
           </ModalFooter>
         </Modal>
@@ -83,17 +97,46 @@ class Recipe extends React.Component {
 	render() {
 		return (
 			<ListGroup>
-				<h1 onClick={this.toggle}>{this.props.recipe.title}</h1>
+				<h1 
+					onClick={this.toggle}
+					className="ing-title">
+					{this.props.recipe.title}
+				</h1>
 				
 					<Collapse isOpen={this.state.collapse}>
 						<h5>Ingredients</h5>
-						
+						<Button
+							color="danger">
+							Delete Recipe
+						</Button>
+						<p/>
 						{this.props.recipe.ingredients.map((ingredient) =>
 							<ListGroupItem>{ingredient}</ListGroupItem>
 						)}
 					</Collapse>
 				
 			</ListGroup>
+		)
+	}
+}
+
+class RecipeForm extends React.Component {
+	render() {
+		return (
+			<Form>
+				<FormGroup>
+					<Label for="recipeTitle">Recipe Title</Label>
+					<Input type="text" name="recipe" id="recipeTitle" placeholder="ex. Spagetti Bolgnese" />
+				</FormGroup>
+				<FormGroup>
+					<Label for="recipeIngredients">Recipe Title</Label>
+					<Input 
+						type="textarea" 
+						name="recipe"
+						id="recipeIngredients" 
+						placeholder="Ingredients seperate by a comma. Ex spagetti, meat balls, mushroooms, bolgnese sauce" />
+				</FormGroup>
+			</Form>
 		)
 	}
 }
